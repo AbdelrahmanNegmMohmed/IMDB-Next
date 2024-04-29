1155ceb8ddd7a8431f40ae16fad9ee5de313feec
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-04-29 06:45:59 +0300

    search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/component/Header";
 import Provider from "./Provider";
 import Navbar from "@/component/Navbar";
+import SearchBox from "@/component/SearchBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Provider>
           <Header />
           <Navbar/>
+          <SearchBox/>
           {children}
         </Provider>
       </body>
